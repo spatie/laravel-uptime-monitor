e4bf897ef93e8ec6c828cbe765dceb722c935036
freek <freek@spatie.be>
2016-11-12 23:09:02 +0100

    commit

---
+++ src/Notifications/Notifications/SoonExpiringSslCertificateFound.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Spatie\UptimeMonitor\Notifications\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackAttachment;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Spatie\UptimeMonitor\Events\SoonExpiringSslCertificateFound as SoonExpiringSslCertificateFoundEvent;
+use Spatie\UptimeMonitor\Notifications\BaseNotification;
+
+class SoonExpiringSslCertificateFound extends BaseNotification
+{
+    /** @var \Spatie\UptimeMonitor\Events\ValidSslCertificateFound */
+    protected $event;
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $mailMessage = (new MailMessage)
+            ->error()
+            ->subject("The certificate for {$this->event->site->url} will expire soon.")
+            ->line("The certificate for {$this->event->site->url} will expire in {$this->ssl_certificate_expiration_date->diffInDays()}");
+
+        return $mailMessage;
+    }
+
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->error()
+            ->content("The certificate for {$this->event->site->url} will expire in {$this->ssl_certificate_expiration_date->diffInDays()}")
+            ->attachment(function (SlackAttachment $attachment) {
+                $attachment->fields($this->getSiteProperties());
+            });
+    }
+
+    public function setEvent(SoonExpiringSslCertificateFoundEvent $event)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+}
