ad9961bfff88c5a08c55d9f168c11e30c6600e4d
freek <freek@spatie.be>
2016-11-15 15:29:11 +0100

    wip

--- tests/Events/SiteDownTest.php
+++ tests/Events/SiteDownTest.php
@@ -43,12 +43,47 @@ class SiteDownTest extends TestCase
         Event::assertFired(SiteDown::class, function ($event) {
             return $event->site->id === $this->site->id;
         });
+    }
 
+    /** @test */
+    public function it_will_fire_the_down_event_again_if_a_site_after_the_configured_amout_of_minutes_if_the_site_stays_down()
+    {
+        $this->server->down();
+
+        $sites = SiteRepository::getAllForUptimeCheck();
+
+        $consecutiveFailsNeeded = config('laravel-uptime-monitor.fire_down_event_after_consecutive_failed_checks');
+
+        foreach(range(1, $consecutiveFailsNeeded) as $index){
+            $sites->checkUptime();
+
+            if ($index < $consecutiveFailsNeeded) {
+                Event::assertNotFired(SiteDown::class);
+            }
+        }
+
+        Event::assertFired(SiteDown::class);
+
+        $this->resetEventAssertions();
+
+        $sites->checkUptime();
+
+        Event::assertNotFired(SiteDown::class);
+
+        $this->resetEventAssertions();
+
+        $this->progressMinutes(config('laravel-uptime-monitor.notifications.resend_down_notification_every_minutes'));
+
+        $sites->checkUptime();
+
+        Event::assertFired(SiteDown::class);
+
+
+    }
+
+    protected function resetEventAssertions()
+    {
         Event::fake();
-
-        Event::assertFired(SiteDown::class, function ($event) {
-            return $event->site->id === $this->site->id;
-        });
     }
 
 
