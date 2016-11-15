5db73b28575dbd4fb1d2bd0aad92ddbeb3d2e8a1
freek <freek@spatie.be>
2016-11-15 19:18:28 +0100

    wip

--- tests/Integration/Models/Traits/SupportsUptimeCheckTest.php
+++ tests/Integration/Models/Traits/SupportsUptimeCheckTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\UptimeMonitor\Test\Integration\Models\Traits;
 
 use Carbon\Carbon;
+use Spatie\UptimeMonitor\Models\Enums\UptimeStatus;
 use Spatie\UptimeMonitor\Models\Site;
 use Spatie\UptimeMonitor\Test\TestCase;
 
@@ -26,7 +27,35 @@ class SupportsUptimeCheckTest extends TestCase
         $this->progressMinutes(config('laravel-uptime-monitor.uptime_check.run_interval_in_minutes') -1);
 
         $this->assertFalse($this->site->shouldCheckUptime());
+
+        $this->progressMinutes(1);
+
+        $this->assertTrue($this->site->shouldCheckUptime());
     }
 
+    /** @test */
+    public function it_will_determine_that_a_site_that_is_down_must_always_be_checked()
+    {
+        $this->site->uptime_status = UptimeStatus::DOWN();
+        $this->site->save();
 
+        foreach(range(1, 10) as $index) {
+            $this->assertTrue($this->site->shouldCheckUptime());
+
+            $this->progressMinutes(1);
+        }
+    }
+
+    /** @test */
+    public function it_will_determine_that_a_site_that_is_not_enabled_must_never_be_checked()
+    {
+        $this->site->enabled = false;
+        $this->site->save();
+
+        foreach(range(1, 10) as $index) {
+            $this->assertFalse($this->site->shouldCheckUptime());
+
+            $this->progressMinutes(1);
+        }
+    }
 }
