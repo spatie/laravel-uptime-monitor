d0a933a4df7cf57f6124a7c986426d3670f080c5
freek <freek@spatie.be>
2016-11-15 19:12:04 -0500

    use trusty

--- tests/server/server.js
+++ tests/server/server.js
@@ -1,7 +1,8 @@
-let app = require('express')();
+"use strict";
 
-let bodyParser = require('body-parser');
+var app = require('express')();
 
+var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
