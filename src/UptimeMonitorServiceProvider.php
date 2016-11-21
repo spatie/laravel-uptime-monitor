0f84c0c86673fc88da52d402d03074c1e43a7be5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-11-21 10:52:55 +0100

    ServiceProvider cleanup

--- src/UptimeMonitorServiceProvider.php
+++ src/UptimeMonitorServiceProvider.php
@@ -11,7 +11,6 @@ use Spatie\UptimeMonitor\Commands\DeleteMonitor;
 use Spatie\UptimeMonitor\Commands\DisableMonitor;
 use Spatie\UptimeMonitor\Commands\EnableMonitor;
 use Spatie\UptimeMonitor\Commands\ListMonitors;
-use Spatie\UptimeMonitor\Models\Monitor;
 use Spatie\UptimeMonitor\Notifications\EventHandler;
 
 class UptimeMonitorServiceProvider extends ServiceProvider
@@ -30,7 +29,6 @@ class UptimeMonitorServiceProvider extends ServiceProvider
         }
 
         if (! class_exists('CreateSitesTable')) {
-            // Publish the migration
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
