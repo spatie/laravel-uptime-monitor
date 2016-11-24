b54c47304c38e83188e20e982dc47381398e44b0
Lukas Kämmerling <lukas.kaemmerling@s-imk.de>
2016-11-24 23:33:53 +0100

    Implement other Tests

--- tests/Integration/API/MonitorControllerTest.php
+++ tests/Integration/API/MonitorControllerTest.php
@@ -22,19 +22,19 @@ class MonitorControllerTest extends TestCase
     public function test_monitor_destroy_api_call()
     {
         $monitor = factory(Monitor::class)->create();
+        $this->assertEquals(1, Monitor::where('url', $monitor->url)->count());
         $this->json('DELETE', route('monitor.destroy', ['monitor' => $monitor->id]))
             ->seeJson([
                 'deleted' => true,
-            ])->seeNotInDatabase('monitors', [
-                'url' => $monitor->url
             ]);
+        $this->assertEquals(0, Monitor::where('url', $monitor->url)->count());
     }
 
     /** @test */
     public function test_monitor_update_api_call()
     {
         $monitor = factory(Monitor::class)->create();
-        $this->json('PUT', route('monitor.update', ['monitor' => $monitor->id], ['url' => 'http://updated.com']))
+        $this->json('PUT', route('monitor.update', ['monitor' => $monitor->id]), ['url' => 'http://updated.com'])
             ->seeJson([
                 'updated' => true,
             ])->seeInDatabase('monitors', [
@@ -45,12 +45,38 @@ class MonitorControllerTest extends TestCase
     /** @test */
     public function test_monitor_get_from_id_api_call()
     {
-
+        $monitor = factory(Monitor::class)->create();
+        $this->json('GET', route('monitor.show', ['monitor' => $monitor->id]))
+            ->seeJsonStructure($monitor);
     }
 
     /** @test */
     public function test_monitor_get_all_api_call()
     {
-
+        factory(Monitor::class, 10)->create();
+        $this->json('GET', route('monitor.index'))
+            ->seeJsonStructure([
+                '*' => [
+                    'id',
+                    'url',
+                    'uptime_check_enabled',
+                    'look_for_string',
+                    'uptime_check_interval_in_minutes',
+                    'uptime_status',
+                    'uptime_check_failure_reason',
+                    'uptime_check_times_failed_in_a_row',
+                    'uptime_status_last_change_date',
+                    'uptime_last_check_date',
+                    'uptime_check_failed_event_fired_on_date',
+                    'uptime_check_method',
+                    'certificate_check_enabled',
+                    'certificate_status',
+                    'certificate_expiration_date',
+                    'certificate_issuer',
+                    'certifcate_check_failure_reason',
+                    'created_at',
+                    'updated_at'
+                ]
+            ]);
     }
 }
