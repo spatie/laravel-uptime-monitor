1600b204e81b43d9ec85c8d58b5c844b7e110aaa
Kostyantyn Bozhko <bks@exyplis.com>
2018-03-13 10:47:15 +0200

    Add option to force run all monitors. Resolves #117. (#136)
    * Add option to force run all monitors. Resolves #117.
    
    * fix typo

--- src/Commands/CheckUptime.php
+++ src/Commands/CheckUptime.php
@@ -7,14 +7,15 @@ use Spatie\UptimeMonitor\MonitorRepository;
 
 class CheckUptime extends BaseCommand
 {
-    protected $signature = 'monitor:check-uptime  
-                            {--url= : Only check these urls}';
+    protected $signature = 'monitor:check-uptime
+                            {--url= : Only check these urls}
+                            {--f|force : Force run all monitors }';
 
     protected $description = 'Check the uptime of all sites';
 
     public function handle()
     {
-        $monitors = MonitorRepository::getForUptimeCheck();
+        $monitors = $this->option('force') ? MonitorRepository::getEnabled() : MonitorRepository::getForUptimeCheck();
 
         if ($url = $this->option('url')) {
             $monitors = $monitors->filter(function (Monitor $monitor) use ($url) {
