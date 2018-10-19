cec515126a79b7181c46836a1446a0251509fda2
Fraser Murray <fraser@gillidanda.co.uk>
2018-10-19 19:19:14 +0100

    Prevent passing undefined variables to compact() for PHP 7.3

--- src/Commands/MonitorLists/Healthy.php
+++ src/Commands/MonitorLists/Healthy.php
@@ -20,6 +20,11 @@ class Healthy
         ConsoleOutput::info('================');
 
         $rows = $healthyMonitor->map(function (Monitor $monitor) {
+            
+            $certificateFound = '';
+            $certificateExpirationDate = '';
+            $certificateIssuer = '';
+
             $url = $monitor->url;
 
             $reachable = $monitor->uptimeStatusAsEmoji;
