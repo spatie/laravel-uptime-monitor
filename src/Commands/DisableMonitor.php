45e8eac40d7be68528668c8dcfb56f455a469c1d
Jan Östlund <janne@eastwest.se>
2020-12-24 07:11:11 +0100

    Update DisableMonitor.php
    Ability to disable all monitors in one go.

--- src/Commands/DisableMonitor.php
+++ src/Commands/DisableMonitor.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\UptimeMonitor\Commands;
 
+use Spatie\UptimeMonitor\Models\Monitor;
 use Spatie\UptimeMonitor\MonitorRepository;
 
 class DisableMonitor extends BaseCommand
@@ -12,14 +13,24 @@ class DisableMonitor extends BaseCommand
 
     public function handle()
     {
-        foreach (explode(',', $this->argument('url')) as $url) {
+        $urls = null;
+
+        if ($this->argument('url') == 'all') {
+            $urls = Monitor::get()->pluck('url');
+        }
+
+        if (is_null($urls)) {
+            $urls = explode(',', $this->argument('url'));
+        }
+
+        foreach ($urls as $url) {
             $this->disableMonitor(trim($url));
         }
     }
 
     protected function disableMonitor(string $url)
     {
-        if (! $monitor = MonitorRepository::findByUrl($url)) {
+        if (!$monitor = MonitorRepository::findByUrl($url)) {
             $this->error("There is no monitor configured for url `{$url}`.");
 
             return;
