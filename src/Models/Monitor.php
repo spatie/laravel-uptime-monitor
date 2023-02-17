d700e9d3cbda86d304aa5b06195f85018baa740d
Laravel Shift <shift@laravelshift.com>
2023-02-17 03:44:52 -0500

    Laravel 10.x Compatibility (#335)
    * Bump dependencies for Laravel 10
    
    * Update GitHub Actions for Laravel 10
    
    * fix for L10
    
    ---------
    
    Co-authored-by: Freek Van der Herten <freek@spatie.be>

--- src/Models/Monitor.php
+++ src/Models/Monitor.php
@@ -23,16 +23,13 @@ class Monitor extends Model
 
     protected $appends = ['raw_url'];
 
-    protected $dates = [
-        'uptime_last_check_date',
-        'uptime_status_last_change_date',
-        'uptime_check_failed_event_fired_on_date',
-        'certificate_expiration_date',
-    ];
-
     protected $casts = [
         'uptime_check_enabled' => 'boolean',
         'certificate_check_enabled' => 'boolean',
+        'uptime_last_check_date' => 'datetime',
+        'uptime_status_last_change_date' => 'datetime',
+        'uptime_check_failed_event_fired_on_date' => 'datetime',
+        'certificate_expiration_date' => 'datetime',
     ];
 
     public function getUptimeCheckAdditionalHeadersAttribute($additionalHeaders): array
