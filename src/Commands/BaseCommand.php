4416819c9aa50e74203548f1deb6bd842705feb8
Jake Barlow <jake@jakebarlow.com>
2023-12-12 12:26:07 -0700

    feat: uses the configured monitor class to sync file

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Spatie\UptimeMonitor\Helpers\ConsoleOutput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Spatie\UptimeMonitor\MonitorRepository;
 
 abstract class BaseCommand extends Command
 {
@@ -15,4 +16,9 @@ abstract class BaseCommand extends Command
 
         return parent::run($input, $output);
     }
+    public function determineHostModelClass()
+    {
+        return MonitorRepository::determineMonitorModel();
+    }
+
 }
